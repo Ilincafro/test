15d3b400761861d6556948c1f005f59f6cd8c4b8
Ilinca <159617576+Ilincafro@users.noreply.github.com>
2024-12-01 10:01:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function openacasa() {
+    window.location.href="index.html";
+}
+function openarticole() {
+    window.location.href="articole.html";
+}
+function openpoze() {
+    window.location.href="poze.html";
+}
